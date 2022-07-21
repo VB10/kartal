b8dd9aac340367b4b8b3ffb6af88fb6bca88f71e
Kadir Bekar <kadir.bekr@gmail.com>
2022-07-21 12:53:40 +0300

    opacity property added for disabled condition toVisible changed from SizedBox to SizedBox.shrink()

--- lib/src/widget_extension.dart
+++ lib/src/widget_extension.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 extension WidgetExtension on Widget {
-  Widget toVisible(bool val, {double? height}) => val ? this : SizedBox(height: height);
-
-  Widget toDisabled([bool? disable]) =>
-      IgnorePointer(ignoring: disable ?? true, child: Opacity(opacity: (disable ?? true) ? 0.2 : 1, child: this));
+  Widget toVisible(bool value) => value ? this : const SizedBox.shrink();
+  Widget toDisabled([bool? disable, double? opacity]) => IgnorePointer(
+        ignoring: disable ?? true,
+        child: Opacity(opacity: (disable ?? true) ? (opacity ?? 0.2) : 1, child: this),
+      );
 }
