f4f97bbbc1d058dbe72afd1c25b3a49fd328415e
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2022-09-05 02:20:22 +0300

    add compute to parsing

--- lib/src/utility/bundle/bundle_decoder.dart
+++ lib/src/utility/bundle/bundle_decoder.dart
@@ -1,9 +1,12 @@
-// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: lines_longer_than_80_chars, omit_local_variable_types, prefer_function_declarations_over_variables
 
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:vexana/vexana.dart';
+
+typedef ComputeCallback<T extends INetworkModel<T>, R> = Future<R> Function(_ComputeArgument<T>);
 
 class BundleDecoder extends _BaseBundleDecoder with _BundleHelpers {
   BundleDecoder(super.assetPath);
@@ -22,17 +25,26 @@ mixin _BundleHelpers {
   Future<R?> loadBundle<T extends INetworkModel<T>, R>(String path, {required T model}) async {
     try {
       final bundle = await rootBundle.loadString(path);
-      return await parse<T, R>(bundle, model: model);
+
+      final ComputeCallback<T, R> callBack = (_ComputeArgument<T> argument) => parse<T, R>(argument);
+
+      return await compute(
+        callBack,
+        _ComputeArgument<T>(
+          bundle: bundle,
+          model: model,
+        ),
+      );
     } catch (e) {
       throw Exception('Error while loading:$e');
     }
   }
 
-  Future<R> parse<T extends INetworkModel<T>, R>(String bundleData, {required T model}) async {
+  Future<R> parse<T extends INetworkModel<T>, R>(_ComputeArgument<T> argument) async {
     if (R == List<T>) {
       try {
-        final transformedData = (json.decode(bundleData) as List<dynamic>) //
-            .map((data) => model.fromJson(data as Map<String, dynamic>)) //
+        final transformedData = (json.decode(argument.bundle) as List<dynamic>) //
+            .map((data) => argument.model.fromJson(data as Map<String, dynamic>)) //
             .cast<T>()
             .toList() as R;
 
@@ -41,7 +53,7 @@ mixin _BundleHelpers {
         throw Exception('Error while parsing:$e');
       }
     } else {
-      return json.decode(bundleData) as R;
+      return json.decode(argument.bundle) as R;
     }
   }
 }
@@ -49,4 +61,11 @@ mixin _BundleHelpers {
 class _BaseBundleDecoder {
   _BaseBundleDecoder(this.assetPath);
   final String assetPath;
+}
+
+class _ComputeArgument<T extends INetworkModel<T>> {
+  _ComputeArgument({required this.bundle, required this.model});
+
+  final String bundle;
+  final T model;
 }
