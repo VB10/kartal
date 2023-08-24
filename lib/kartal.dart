1636744bf75b49aeb2800963db250e153f843186
vb10 <vbacik.10@gmail.com>
2023-08-24 03:11:51 +0300

    updated utility index

--- lib/kartal.dart
+++ lib/kartal.dart
@@ -14,8 +14,5 @@ export 'src/index.dart';
 export 'src/int_extension.dart';
 export 'src/list_extension.dart';
 export 'src/string_extension.dart';
-export 'src/utility/bundle/bundle_decoder.dart';
-export 'src/utility/device_utility.dart';
 export 'src/utility/index.dart';
-export 'src/utility/page_animation/slider_route.dart';
 export 'src/widget_extension.dart';
