7377c612375418e7510223632e2d83553ca72db3
vb10 <vbacik.10@gmail.com>
2024-03-28 12:54:22 +0300

    Refactor int_extension.dart to use private class for additional functionalities

--- lib/src/int_extension.dart
+++ lib/src/int_extension.dart
@@ -3,10 +3,23 @@ import 'dart:ui';
 
 import 'package:kartal/src/utility/http_result.dart';
 
-part './private/integer_ext.dart';
-
 /// Extension methods for [int] to provide additional functionalities.
 extension IntegerExtension on int {
   /// Provides convenient access to additional functionalities for [int].
-  IntegerExt get ext => IntegerExt(this);
+  _IntegerExt get ext => _IntegerExt(this);
+}
+
+final class _IntegerExt {
+  _IntegerExt(int value) : _value = value;
+
+  final int _value;
+
+  /// Returns a random color value between 0 and 255.
+  int get randomColorValue => Random(_value).nextInt(256);
+
+  /// Returns a [HttpResult] based on the status code.
+  HttpResult get httpStatus => HttpResult.fromStatusCode(_value);
+
+  /// Returns a [Color] based on the status code.
+  Color get httpStatusColor => httpStatus.color;
 }
